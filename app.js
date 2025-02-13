cf0f2c0aef772e0a818de35fe5773cacca9ed7ba
Jyrenn <clement.martin.27@gmail.com>
2025-02-13 17:09:49 +0100

    Mise en place Backend + authentification

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bookRoutes = require("./routes/book");
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(
+    "mongodb+srv://Jyrenn:Bleach2710.@cluster0.elo26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,5 +25,8 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use("/api/books", bookRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
